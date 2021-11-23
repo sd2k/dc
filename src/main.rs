acbb8a6664a2b7351c436a331a818901355e3d89
Ben Sully <ben.sully@grafana.com>
2021-11-23 16:18:40 +0000

    Support docker-compose.override.yml

--- src/main.rs
+++ src/main.rs
@@ -14,22 +14,32 @@ use tracing_subscriber::{fmt, EnvFilter};
 
 const RECURSE_INTO_DIRS: [&str; 1] = ["docker-compose"];
 const VALID_FILENAMES: [&str; 2] = ["docker-compose.yml", "docker-compose.yaml"];
+const VALID_OVERRIDE_FILENAMES: [&str; 2] = [
+    "docker-compose.override.yml",
+    "docker-compose.override.yaml",
+];
 
 /// Returns the name of the compose file in the supplied, if it exists.
 #[instrument]
-fn get_compose_file(path: &Path) -> Result<Option<Cow<'static, str>>> {
+fn get_compose_file(
+    path: &Path,
+    valid_filenames: &'static [&'static str],
+) -> Result<Option<Cow<'static, str>>> {
     if path.is_dir() {
         for entry in read_dir(path)?.flatten() {
             if let Some(name) = entry.file_name().to_str() {
-                for x in VALID_FILENAMES {
-                    if x == name {
+                for x in valid_filenames {
+                    if x == &name {
                         return Ok(Some(Cow::Borrowed(x)));
                     }
                 }
                 for x in RECURSE_INTO_DIRS {
                     if x == name {
-                        if let Some(file) = find_compose_file(entry.path(), SearchDepth::Limited(1))
-                        {
+                        if let Some(file) = find_compose_file(
+                            entry.path(),
+                            SearchDepth::Limited(1),
+                            valid_filenames,
+                        ) {
                             return Ok(Some(Cow::Owned(
                                 file.into_os_string().into_string().unwrap(),
                             )));
@@ -60,11 +70,15 @@ impl SearchDepth {
 /// Searches for a docker-compose file, starting in the supplied directory
 /// and working upwards up to `max_depth` levels.
 #[instrument]
-fn find_compose_file(mut path: PathBuf, max_depth: SearchDepth) -> Option<PathBuf> {
+fn find_compose_file(
+    mut path: PathBuf,
+    max_depth: SearchDepth,
+    valid_filenames: &'static [&'static str],
+) -> Option<PathBuf> {
     let mut depth = 0;
     loop {
         debug!("Searching in {}", path.to_str().unwrap());
-        if let Ok(Some(filename)) = get_compose_file(&path) {
+        if let Ok(Some(filename)) = get_compose_file(&path, valid_filenames) {
             path.push(filename.as_ref());
             return Some(path);
         }
@@ -83,18 +97,26 @@ fn find_compose_file(mut path: PathBuf, max_depth: SearchDepth) -> Option<PathBu
 /// Runs the supplied docker-compose command with the '-f' flag included.
 #[instrument]
 fn run_command(args: ArgsOs) -> Result<Child> {
-    let compose_file =
-        find_compose_file(current_dir()?, SearchDepth::Unlimited).ok_or_else(|| {
+    let compose_file = find_compose_file(current_dir()?, SearchDepth::Unlimited, &VALID_FILENAMES)
+        .ok_or_else(|| {
             eyre!(
         "Couldn't find a docker-compose.yml or docker-compose.yaml file in any parent directory!"
     )
             .suggestion("Make sure you're in a project with a docker-compose file.")
         })?;
-    Ok(Command::new("docker-compose")
-        .arg("-f")
-        .arg(compose_file)
-        .args(args.skip(1))
-        .spawn()?)
+    let override_file = find_compose_file(
+        current_dir()?,
+        SearchDepth::Unlimited,
+        &VALID_OVERRIDE_FILENAMES,
+    );
+    let mut cmd = Command::new("docker-compose");
+    cmd.arg("-f").arg(compose_file);
+    if let Some(f) = override_file {
+        cmd.arg("-f").arg(f);
+    }
+    cmd.args(args.skip(1));
+    debug!(command = ?cmd, "Running command");
+    Ok(cmd.spawn()?)
 }
 
 fn install_tracing() {
